ca5f6d44bf6d2aabb34aab0afd56e0e4ce75b87c
gopichandu413 <jujjavarapugopichandu@gmail.com>
2024-12-31 11:59:53 +0530

    File_Upload

---
+++ application1/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+def Test_case1(request):
+    return render(request,"application1/S1.html")
---
+++ static/js/demo1.js
@@ -0,0 +1,4 @@
+function Test_case1()
+{
+    window.alert("Welocome to JGC Softwares")
+}
